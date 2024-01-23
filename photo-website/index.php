7d312eadfcad13a4bf71deb838a2512eaf8cf76d
Luke <35667@ma-web.nl>
2024-01-23 10:58:45 +0100

    frontend voor foto website

---
+++ photo-website/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fotos</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <h1 class="fotostitle">Kunstspeeltuin Foto's</h1>
+    <article class="fotosarticle">
+        <section class="fotossection">
+            <figure class="fotosfigure">
+                <img src="" alt="" class="fotosimg">
+            </figure>
+        </section>
+    </article>
+</body>
+</html>
